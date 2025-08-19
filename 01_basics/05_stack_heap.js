ea63191e427f6fb50f262c26ef3b9299bdbb8377
Shubham Thakur <st62586@gmail.com>
2025-08-19 09:35:06 +0000

    modified stack heap

--- 01_basics/05_stack_heap.js
+++ 01_basics/05_stack_heap.js
@@ -1,2 +1,28 @@
+// Primitve data ----> stack memory
+
 let Name = "shubham"
-let 
+let anotherName = Name
+/* let anotherName = "Thakur" You are declaring anotherName twice with let.
+   In JavaScript, you cannot redeclare a variable in the same scope with let or const.*/
+
+anotherName="Thakur"       // // ✅ reassignment, not redeclaration
+
+console.log(Name)       // shubham   
+console.log(anotherName)  // thakur
+console.log(Name)        // shubham
+
+/* Copy by Value concept is applied here, where original value doesnt change, rather it created another copy */
+
+// Non-primitive ----> heap memory
+
+let userOne = {
+    email:"st62586@gmail.com",
+    phoneno: 7999483191
+
+}
+
+let userTwo = userOne
+userTwo.email("shubhamt2720@gmail.com")
+
+console.log("userOne")
+console.log("userTwo")
